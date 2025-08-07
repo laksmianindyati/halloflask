7fb52ef41d79ea713071c5414e62c09ecfb3c6e4
laksmi an <37676058+laksmianindyati@users.noreply.github.com>
2025-08-07 15:26:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, test to google cloud!"
 
 if __name__ == "__main__":
     app.run(debug=True)
